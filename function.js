87503d6a59e0b44dd4a6bec875f5fceb230251bc
Preetiranjan Sahoo <preetiranjansahoo388@gmail.com>
2024-09-04 23:49:21 +0530

    completed

---
+++ function.js
@@ -0,0 +1,31 @@
+// function me(){
+//     console.log('hello world');
+// }
+// me();
+
+
+// function me(name){
+//     console.log('hello ' + name);
+// }
+// me('banty');
+// me('Bablu');
+
+// type of function
+
+// function square(number){
+//     return number * number;
+// }
+// let number=square(2);
+// console.log(number);
+
+
+// function greet(a,b){
+//     console.log(a+b);
+// }
+// greet(1,2);
+
+
+// function greet(){
+//     console.log('hello banty');
+// }
+// greet();
