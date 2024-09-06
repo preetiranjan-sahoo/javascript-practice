0c78549a81a4cfeeff0f50e510ee4ba69f61994f
Preetiranjan Sahoo <preetiranjansahoo388@gmail.com>
2024-09-06 22:36:52 +0530

    add arrow function

--- function.js
+++ function.js
@@ -33,4 +33,42 @@
 // function sum(a,b){
 //     return a+b;
 // }
-// console.log(sum(2,4));
+// console.log(sum(2,5));
+
+
+// const calc = (num1,num2,operator) => {
+//     let result;
+//     switch(operator){
+//         case '+':
+//             result =num1+num2;
+//             return result;
+//             case '-':
+//                 result=num1-num2;
+//                 return result;;
+//                 case '*':
+//                     result=num1*num2;
+//                     return result;;
+//                     case '/':
+//                         result=num1/num2;
+//                         return result;;
+//                         default:
+//                             result='invalid operator';
+//                             return result;;
+//     }
+//     }
+//     console.log(calc(3,4,"-"));
+
+
+
+//reverse string using fat arrow function
+// const reverseString = (str) => {
+   // return str.split("").reverse().join("");
+
+//    let reverse = "";
+//    for(let char=str.length-1;char>=0;char--){
+//     reverse=reverse+str[char];
+//    }
+//    return reverse;
+
+// };
+// console.log(reverseString("hello banty"));
