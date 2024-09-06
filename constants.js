a3833d0846dca47b21380e4065e538ce0a14adb5
Preetiranjan Sahoo <preetiranjansahoo388@gmail.com>
2024-09-06 21:46:32 +0530

    let

---
+++ constants.js
@@ -0,0 +1,63 @@
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+
+// var interestRate = "Banty";
+// interestRate = "Bablu";
+// console.log(interestRate);
+
+
+// const interestRate = "Banty";
+// interestRate = "Bablu";
+// console.log(interestRate);
+
+
+// var name="Banty";
+// if(true){
+//     var name="Bablu";
+//     console.log(name);
+// }
+// name="Preetiranjan";
+// console.log(name);
+
+
+// let name="Banty";
+// if(true){
+//     let name="Bablu";
+//     console.log(name);
+// }
+// name="Preetiranjan";
+// console.log(name);
+
+
+// const name="Banty";
+// if(true){
+//     const name="Bablu";
+//     console.log(name);
+// }
+// name="Preetiranjan";
+// console.log(name);
+
+
+// if(true){
+//     var name="Bablu";
+//     console.log(name);
+// }
+// // name="Preetiranjan";
+// console.log(name);
+
+
+// if(true){
+//     let name="Bablu";
+//     console.log(name);
+// }
+// //name="Preetiranjan";
+// console.log(name);
+
+
+let firstName="banty";
+let lastName="Bablu";
+// let fullName= firstName + lastName;
+let fullName= `${firstName} ${lastName}`;
+console.log(fullName);
