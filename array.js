61c06a8354c070234ababdd5aeb1e820ea48383a
Preetiranjan Sahoo <preetiranjansahoo388@gmail.com>
2024-09-08 20:02:07 +0530

    completed

---
+++ array.js
@@ -0,0 +1,90 @@
+// let arr=[];
+// console.log(typeof arr);
+
+
+// let name=["banty","ayush","babuna","sipu"];
+// console.log(name);
+// console.log(name[2]);
+// name[2]="jd";
+// console.log(name[2]);
+
+//for
+// let name=["banty","ayush","babuna","sipu"];
+// for(let arr=0;arr<name.length;arr++ ){
+//     console.log(name[arr])
+// }
+
+//for of
+// let marks=[85,97,44,37,76,60];
+// for(let nums of marks)
+// {
+//     console.log(nums);
+// }
+
+
+//for each
+// let name=["banty","ayush","babuna","sipu"];
+// name.forEach((elm, index, arr) => {
+//     // console.log(`${elm} ${index}`);
+//     console.log(arr);
+// });
+
+//map
+// let name=["banty","ayush","babuna","sipu"];
+// name.map((elm, index, arr) => {
+//         console.log(`${elm} ${index}`);
+//         // console.log(arr);
+//     });
+
+
+// let nums=[1,2,3,4,5];
+// nums.forEach((elm) => {
+// console.log(elm*2);
+// });
+
+
+// let name=["banty","ayush","babuna","sipu"];
+
+//push
+// name.push("sudesh");
+// console.log(name);
+
+//pop
+// name.pop();
+// console.log(name);
+
+//unshift
+// name.unshift("sudesh");
+// console.log(name);
+
+//shift
+// name.shift();
+// console.log(name);
+
+
+//splice
+//array.splice(index, count, item1, ....., itemX)
+
+// console.log(name.splice(1, 1, "muu"));
+// console.log(name);
+
+
+// let name=["banty","ayush","babuna","sipu","babuna"];
+// console.log(name.indexOf("babuna"));
+// console.log(name.lastIndexOf("babuna"));
+
+// const products = [
+//     {name : "laptop", price:1200},
+//     {name : "mobile", price:800},
+//     {name : "watch", price:500},
+//     {name : "watch", price:500},
+// ];
+// const filterProducts = products.filter((curElm) =>{
+//     return curElm.price < 800;
+// });
+// console.log(filterProducts);
+
+//sort an array
+// let name=["banty","ayush","babuna","sipu","babuna"];
+// name.sort();
+// console.log(name);
