2c9bbe0ffe128cdbf1dd7bc6b8efe74151c2d7ca
Preetiranjan Sahoo <preetiranjansahoo388@gmail.com>
2024-09-08 20:08:12 +0530

    to be continued

---
+++ string.js
@@ -0,0 +1,8 @@
+// let fullName = prompt("enter fullName without spaces");
+
+// let username = "@" + fullName + fullName.length;
+// console.log(username);
+
+// const str= 'hello world';
+// console.log(str.length);
+
