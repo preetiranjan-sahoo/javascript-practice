6f763a5bb0b812b865fa0a73e748293d47f38dd1
Preetiranjan Sahoo <preetiranjansahoo388@gmail.com>
2024-09-23 19:03:42 +0530

    completed

--- array.js
+++ array.js
@@ -90,4 +90,68 @@
 // console.log(name);
 
 
-//to be continue 
+//includes
+
+// const nums = [1,2,3,4,5,6,7,8,9];
+// const result = nums.includes(1);
+// console.log(result);
+
+//add dec in end of array using splice
+// const months=[",jan","march","april","june"];
+// months.push("dec");
+// months.splice(months.length,0,"dec");
+// console.log(months);
+
+//update march to March
+// months.splice(1,1,"March");
+// console.log(months);
+
+//delete jan from array
+// months.splice(0,1);
+// console.log(months);
+
+
+//filtering products by price
+// const products = [
+//     {name : "laptop", price:1200},
+//     {name : "mobile", price:800},
+//     {name : "watch", price:500},
+//     {name : "watch", price:500},
+//     {name : "tablet", price:150},
+// ];
+//filter products with a price less than or equal to 500
+// const filteredProducts = products.filter((product) =>{
+//     return product.price <= 500;
+// });
+// console.log(filteredProducts);
+
+
+//filter unique values
+// const numbers = [1,2,2,3,4,4,5,6];
+// const uniqueNumbers = [...new Set(numbers)];
+// console.log(uniqueNumbers);
+// const uniqueNumbers = numbers.filter((num, index, array) => {
+//     return array.indexOf(num) === index;
+//     });
+//     console.log(uniqueNumbers);
+
+
+//sort and compare an array
+// const numbers = [1,6,2,3,4,1,5,6,7,8];
+// const sortedNumbers = [...numbers].sort((a,b) =>{
+//     return a - b;
+//     });
+//     console.log(sortedNumbers);
+
+// const sortedNumbers = numbers.sort((a,b) =>{
+//     return a - b;
+//     });
+//     console.log(sortedNumbers);
+
+
+//write a function that calculate the total price of items in a shoping cart. the function shouuld take an array of item prices as input and return the total price.
+// const price =[100,200,300,400,500];
+// const totalPrice = price.reduce((acc, current) => {
+//     return acc + current;
+//     }, 0);
+//     console.log(totalPrice);
